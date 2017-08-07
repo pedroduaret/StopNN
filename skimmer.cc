a48b4d7390eff6c253c5c588e7e5ae38184290e2
Cristóvão Beirão da Cruz e Silva <cbeiraod@cern.ch>
2017-08-07 14:37:59 +0100

    Updated the skimmer (hopefully no bugs) so that the copy is more inteligent and that it updates the weight branch in the process

--- skimmer.cc
+++ skimmer.cc
@@ -13,15 +13,38 @@ int skimmer(std::string fileToProcess)
   TFile inFile((fileToProcess + ".root").c_str(), "READ");
   TFile outFile((fileToProcess + "_skimmed.root").c_str(), "RECREATE");
 
-  TTree* tmp = static_cast<TTree*>(inFile.Get("bdttree"));
-  tmp->SetBranchStatus("*",1);
-  tmp->SetBranchStatus("*_Up",0);
-  tmp->SetBranchStatus("*_Down",0);
+  TTree* inTree = static_cast<TTree*>(inFile.Get("bdttree"));
+  inTree->SetBranchStatus("*",1);
+  inTree->SetBranchStatus("*_Up",0);
+  inTree->SetBranchStatus("*_Down",0);
 
-  TTree* newTree = tmp->CloneTree(75000);
-  //TTree* newTree = tmp->CloneTree();
+  TTree* tmp = inTree->CloneTree(75000);
+  tmp->SetBranchStatus("*", 1);
+  tmp->SetBranchStatus("weight", 0);
+  TTree* outTree = tmp.CloneTree();
 
-  newTree->Write();
+  Float_t weight = 0;
+  tmp->SetBranchAddress("weight", &weight);
+  TBranch* newWeightBranch = outTree->Branch("weight", &weight);
+
+  Long64_t inEvents = inTree->GetEntries();
+  Long64_t copyEvents = tmp->GetEntries();
+  Float_t factor = 1;
+  if(copyEvents < inEvents)
+  {
+    factor = inEvents;
+    factor = factor / copyEvents;
+  }
+
+  for(Long64_t i = 0; i < copyEvents; ++i)
+  {
+    tmp->GetEntry(i);
+    weight = weight * factor;
+    newWeightBranch->Fill();
+  }
+
+
+  outTree->Write();
 
   return 0;
 }
