60e7a43a5e00a673a80568051481ef9f8e2f0560
Cristóvão Beirão da Cruz e Silva <cbeiraod@cern.ch>
2017-08-11 16:00:03 +0100

    Created the commonFunctions file with a function to load the data

--- trainNN.py
+++ trainNN.py
@@ -13,30 +13,7 @@ from keras.optimizers import Adam
 from sklearn.metrics import confusion_matrix, cohen_kappa_score
 
 import localConfig as cfg
-
-treeName = "bdttree"
-baseSigName = "T2DegStop_300_270"
-bkgDatasets = [
-                "Wjets_70to100",
-                "Wjets_100to200",
-                "Wjets_200to400",
-                "Wjets_400to600",
-                "Wjets_600to800",
-                "Wjets_800to1200",
-                "Wjets_1200to2500",
-                "Wjets_2500toInf",
-                "TTJets_DiLepton",
-                "TTJets_SingleLeptonFromTbar",
-                "TTJets_SingleLeptonFromT",
-                "ZJetsToNuNu_HT100to200",
-                "ZJetsToNuNu_HT200to400",
-                "ZJetsToNuNu_HT400to600",
-                "ZJetsToNuNu_HT600to800",
-                "ZJetsToNuNu_HT800to1200",
-                "ZJetsToNuNu_HT1200to2500",
-                "ZJetsToNuNu_HT2500toInf"
-
-              ]
+from commonFunctions import StopDataLoader
 
 myFeatures = ["Jet1Pt", "Met", "Njet", "LepPt", "LepEta", "LepChg", "HT", "NbLoose"]
 inputBranches = list(myFeatures)
@@ -44,109 +21,25 @@ inputBranches.append("XS")
 inputBranches.append("weight")
 preselection = "(DPhiJet1Jet2 < 2.5 || Jet2Pt < 60) && (Met > 280) && (HT > 200) && (isTight == 1) && (Jet1Pt > 110)"
 suffix = "_skimmed"
+luminosity = 35866
 
 print "Loading datasets..."
-sigDataDev = pandas.DataFrame(root_numpy.root2array(
-                                                    cfg.loc + "/train/" + baseSigName + suffix + ".root",
-                                                    treename=treeName,
-                                                    selection=preselection,
-                                                    branches=inputBranches
-                                                    ))
-sigDataVal = pandas.DataFrame(root_numpy.root2array(
-                                                    cfg.loc + "/test/" + baseSigName + suffix + ".root",
-                                                    treename=treeName,
-                                                    selection=preselection,
-                                                    branches=inputBranches
-                                                    ))
-bkgDataDev = None
-bkgDataVal = None
-for bkgName in bkgDatasets:
-  if bkgDataDev is None:
-    bkgDataDev = pandas.DataFrame(root_numpy.root2array(
-                                                        cfg.loc + "/train/" + bkgName + suffix + ".root",
-                                                        treename=treeName,
-                                                        selection=preselection,
-                                                        branches=inputBranches
-                                                        ))
-    bkgDataVal = pandas.DataFrame(root_numpy.root2array(
-                                                        cfg.loc + "/test/" + bkgName + suffix + ".root",
-                                                        treename=treeName,
-                                                        selection=preselection,
-                                                        branches=inputBranches
-                                                        ))
-  else:
-    bkgDataDev = bkgDataDev.append(
-                                   pandas.DataFrame(root_numpy.root2array(
-                                                                          cfg.loc + "/train/" + bkgName + suffix + ".root",
-                                                                          treename=treeName,
-                                                                          selection=preselection,
-                                                                          branches=inputBranches
-                                                                          )),
-                                   ignore_index=True
-                                   )
-    bkgDataVal = bkgDataVal.append(
-                                   pandas.DataFrame(root_numpy.root2array(
-                                                                          cfg.loc + "/test/" + bkgName + suffix + ".root",
-                                                                          treename=treeName,
-                                                                          selection=preselection,
-                                                                          branches=inputBranches
-                                                                          )),
-                                   ignore_index=True
-                                   )
-
-sigDataDev["category"] = 1
-sigDataVal["category"] = 1
-bkgDataDev["category"] = 0
-bkgDataVal["category"] = 0
-sigDataDev["sampleWeight"] = 1
-sigDataVal["sampleWeight"] = 1
-bkgDataDev["sampleWeight"] = 1
-bkgDataVal["sampleWeight"] = 1
-
-# Calculate event weights
-# The input files already have a branch called weight, which contains the per-event weights
-# These precomputed weights have all scale factors applied. If we desire to not use the scale factors
-# we should compute a new set of weights ourselves. Remember to repeat for all datasets.
-################### Add computation here if wanted #######################################################
-# After computing the weights, the total class has to be normalized.
-sigDataDev.sampleWeight = sigDataDev.weight/sigDataDev.weight.sum()
-sigDataVal.sampleWeight = sigDataVal.weight/sigDataVal.weight.sum()
-bkgDataDev.sampleWeight = bkgDataDev.weight/bkgDataDev.weight.sum()
-bkgDataVal.sampleWeight = bkgDataVal.weight/bkgDataVal.weight.sum()
-
-
-data = sigDataDev.copy()
-data = data.append(sigDataVal.copy(), ignore_index=True)
-data = data.append(bkgDataDev.copy(), ignore_index=True)
-data = data.append(bkgDataVal.copy(), ignore_index=True)
-print 'Datasets contain a total of', len(data), 'events:'
-print '  Signal:'
-print '    Development (train):', len(sigDataDev)
-print '    Validation (test):', len(sigDataVal)
-print '  Background:'
-print '    Development (train):', len(bkgDataDev)
-print '    Validation (test):', len(bkgDataVal)
+dataDev, dataVal = StopDataLoader(cfg.loc, inputBranches, selection=preselection, suffix=suffix, signal="300_270")
+#print dataDev.describe()
+#print dataVal.describe()
+data = dataDev.copy()
+data = data.append(dataVal.copy(), ignore_index=True)
+print 'Datasets contain a total of', len(data), '(', data.weight.sum()*luminosity, 'weighted) events:'
+print '  Development (train):', len(dataDev), '(', dataDev.weight.sum()*luminosity, 'weighted)'
+print '    Signal:', len(dataDev[dataDev.category == 1]), '(', dataDev[dataDev.category == 1].weight.sum()*luminosity, 'weighted)'
+print '    Background:', len(dataDev[dataDev.category == 0]), '(', dataDev[dataDev.category == 0].weight.sum()*luminosity, 'weighted)'
+print '  Validation (test):', len(dataVal), '(', dataVal.weight.sum()*luminosity, 'weighted)'
+print '    Signal:', len(dataVal[dataVal.category == 1]), '(', dataVal[dataVal.category == 1].weight.sum()*luminosity, 'weighted)'
+print '    Background:', len(dataVal[dataVal.category == 0]), '(', dataVal[dataVal.category == 0].weight.sum()*luminosity, 'weighted)'
 
 print 'Finding features of interest'
 trainFeatures = [var for var in data.columns if var in myFeatures]
 otherFeatures = [var for var in data.columns if var not in trainFeatures]
-
-print "Filtering the features of interest"
-tmpList = list(trainFeatures) # To create a real copy
-tmpList.append("category") # Add to tmpList any columns that really are needed, for whatever reason
-tmpList.append("weight")
-tmpList.append("sampleWeight")
-data = data[tmpList]
-
-dataDev = sigDataDev[tmpList].copy()
-dataDev = dataDev.append(bkgDataDev[tmpList].copy(), ignore_index=True)
-
-dataVal = sigDataVal[tmpList].copy()
-dataVal = dataVal.append(bkgDataVal[tmpList].copy(), ignore_index=True)
-
-print data.describe()
-print dataDev.describe()
-print dataVal.describe()
 
 ######################################
 
